3583b7c82293f6fe996794a50a6e4adf0a703750
DESKTOP-7GPI486\kseni <ksenia.gladkikh@outlook.com>
2021-05-12 17:49:50 +0200

    Edited Search method in AccomodationController (now returns status 404 in case destination doesn't exist)

--- Holiday_Maker/Controllers/AccomodationsController.cs
+++ Holiday_Maker/Controllers/AccomodationsController.cs
@@ -133,15 +133,15 @@ namespace Holiday_Maker.Controllers
 
         private SearchFunctionService _searchFunctionService = new SearchFunctionService();
 
-        [HttpPut]
-        public async Task<IEnumerable<Accomodation>> Search(string country, string city)
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<Accomodation>>> Search(string country, string city)
         {
             var accomodation = _searchFunctionService.SearchAccomodationByCountryAndCity(country, city);
             if(accomodation !=null)
             {
                 return await accomodation.ToListAsync();
             }
-            return new List<Accomodation>();
+            return NotFound();
         }
     }
 }
