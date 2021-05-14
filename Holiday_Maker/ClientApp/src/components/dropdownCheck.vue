4a980cd900a4a1f5c5640dd5ce5bd91840d0a3b8
ThomasLohr <73110720+ThomasLohr@users.noreply.github.com>
2021-05-14 09:47:01 +0200

    added a dropdown and space between checkboxes

--- Holiday_Maker/ClientApp/src/components/dropdownCheck.vue
+++ Holiday_Maker/ClientApp/src/components/dropdownCheck.vue
@@ -1,39 +1,66 @@
 <template>
-  <div class="filter-box">
-    <p>Tick an option</p>
-    <div>
-      <div>
+<div class="filter-box">
+    <div class="multipleSelection">
+      <div class="selectBox" @click="toggledisplay">
+        <select>
+          <option>Select extras</option>
+        </select>
+        <div class="overSelect"></div>
+      </div>
+
+    <div :class="{show : toggle}" id="checkboxes">
         <div class="flex items-center">
-          <input type="checkbox" id="checkbox" v-model="checked" />
-          <label for="checkbox">Self-Catering</label>
+          <input type="checkbox" id="first"/>
+          <label for="first">&nbsp;Self-Catering</label>
         </div>
         <div class="flex items-center">
-          <input type="checkbox" id="checkbox" v-model="checked" />
-          <label for="checkbox">Halfboard</label>
+          <input type="checkbox" id="second"/>
+          <label for="second">&nbsp;Halfboard</label>
         </div>
         <div>
           <div class="flex items-center">
-            <input type="checkbox" id="checkbox" v-model="checked" />
-            <label for="checkbox">Fullboard</label>
+            <input type="checkbox" id="second"/>
+            <label for="second">&nbsp;Fullboard</label>
           </div>
         </div>
         <div class="flex items-center">
-          <input type="checkbox" id="checkbox" v-model="checked" />
-          <label for="checkbox">All-inclusive</label>
+          <input type="checkbox" id="third"/>
+          <label for="third">&nbsp;All-inclusive</label>
         </div>
         <div class="flex items-center">
-          <input type="checkbox" id="checkbox" v-model="checked" />
-          <label for="checkbox">Breakfast</label>
+          <input type="checkbox" id="fourth"/>
+          <label for="fourth">&nbsp;Breakfast</label>
         </div>
         <div class="flex items-center">
-          <input type="checkbox" id="checkbox" v-model="checked" />
-          <label for="checkbox">Extrabed</label>
+          <input type="checkbox" id="fifth"/>
+          <label for="fifth">&nbsp;Extrabed</label>
         </div>
         <div class="flex items-center">
-          <input type="checkbox" id="checkbox" v-model="checked" />
-          <label for="checkbox">Crib</label>
+          <input type="checkbox" id="sixth"/>
+          <label for="sixth">&nbsp;Crib</label>
         </div>
       </div>
     </div>
   </div>
+  
 </template>
+
+   <script>
+
+export default{
+  data(){
+    return{
+    toggle: true,
+  
+    
+    }
+  },
+  methods:{
+    toggledisplay(){
+      this.toggle=!this.toggle
+    
+    }
+  }
+}
+
+</script>
