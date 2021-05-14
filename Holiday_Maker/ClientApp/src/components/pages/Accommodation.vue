3cdf976a7d769d284b36aca7a0e7f3a1c1923767
Christoffer Jörgensen <christoffer.joergensen@gmail.com>
2021-05-14 12:38:56 +0200

    Added details page for accommodations, including dynamic routing. Also added a NotFound page.

---
+++ Holiday_Maker/ClientApp/src/components/pages/Accommodation.vue
@@ -0,0 +1,40 @@
+<template>
+
+<div class="col-md-12" v-for="accomodation in accomodations" :key="accomodation">
+     <div v-if="id == accomodation.id">
+
+
+<div class="flex flex-wrap overflow-hidden">
+
+  <div class="w-full overflow-hidden xl:w-full">
+    <b>{{ accomodation.name }}</b>
+  </div>
+
+  <div class="w-full overflow-hidden xl:w-full">
+    <img :src="accomodation.imgSrc" class="img-fluid">
+  </div>
+
+  <div class="w-full overflow-hidden xl:w-full">
+    {{ accomodation.description }}
+  </div>
+
+</div>
+
+
+
+     </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+  props: ["id"],
+
+  computed: {
+    accomodations() {
+      return this.$store.state.accomodations;
+    },
+  },
+};
+</script>
--- Holiday_Maker/ClientApp/src/router/index.js
+++ Holiday_Maker/ClientApp/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Hotels from '/src/components/pages/Hotels.vue'
 import StoreTest from '/src/components/pages/StoreTest.vue'
+import Accommodation from '/src/components/pages/Accommodation.vue'
+import NotFound from '/src/components/pages/NotFound.vue'
+
 const routes = [
     {
         path: '/hotels',
@@ -11,7 +14,17 @@ const routes = [
         path: '/storetest',
         name: 'StoreTest',
         component: StoreTest,
-    }
+    },
+    {
+        path: "/accommodation/:id",  // <-- notice the colon
+        name: "Accommodation",
+        component: Accommodation,
+        props: true,
+      },
+      {
+  path: "/:catchAll(.*)",
+  component: NotFound,
+},
 ]
 const router = createRouter({
     history: createWebHistory(),
