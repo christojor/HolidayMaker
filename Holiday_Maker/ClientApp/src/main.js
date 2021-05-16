bc2263f4b7f02b0ec6350cc3efa9b98654a121f4
Christoffer Jörgensen <christoffer.joergensen@gmail.com>
2021-05-17 01:25:39 +0200

    Added custom alertbox.

--- Holiday_Maker/ClientApp/src/main.js
+++ Holiday_Maker/ClientApp/src/main.js
@@ -6,11 +6,16 @@ import store from "./store/index"
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faBaby, faBed, faCoffee, faHamburger, faMinusSquare, faPhone, faPlusSquare, faSquare, faStar, faSwimmer, faSwimmingPool, faUserSecret } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import AlertBox from "./components/errors/AlertBox.vue";
+
+
+
 
 library.add(faStar, faCoffee, faSwimmer, faHamburger, faPlusSquare, faSquare, faBed, faMinusSquare, faBaby);
 
 createApp(App)
 .component("font-awesome-icon", FontAwesomeIcon)
-.use(store).
-use(router).
-mount('#app')
+.component("AlertBox", AlertBox) //Globally registered component
+.use(store)
+.use(router)
+.mount('#app')
