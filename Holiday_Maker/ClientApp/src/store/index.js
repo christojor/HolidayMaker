eed566e451fb62541569c6b02e88d8cc094058e3
Geckk0 <jesper95951@gmail.com>
2021-05-17 09:13:53 +0200

    Removed unused code

--- Holiday_Maker/ClientApp/src/store/index.js
+++ Holiday_Maker/ClientApp/src/store/index.js
@@ -11,7 +11,6 @@ const store = createStore({
         information: "This is just some random text.",
         message: 'Hello Vuex',
         accomodations: [],
-        filteredAccomodations: [],
         filter: []
    },
 
