4080b6889b57c5eb4b63285f778d77060c48ef4f
Johan Rova <johan.rova@protonmail.com>
2021-05-19 14:49:14 +0200

    PaymentIntent test is now working, api calls return 200 and result. Added paymentcontroller, paymentservice as well as Stripe.Net package. Changed gitignore to exclude secrets.json, added secrets class to structure secrets in future.

---
+++ Holiday_Maker/Controllers/PaymentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Holiday_Maker.Services;
+using Stripe;
+
+namespace Holiday_Maker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private PaymentService _paymentService;
+        public PaymentController()
+        {
+            _paymentService = new PaymentService();
+        }
+        // GET: api/PaymentIntent
+        [HttpPost("PaymentIntent")]
+        public async Task<string> PaymentIntent()
+        {
+            return _paymentService.MakePayment().ToJson();
+        }
+    }
+}
---
+++ Holiday_Maker/Secret.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Holiday_Maker
+{
+    /// <summary>
+    /// A class that shows the structure of secrets inside this project. The secrets are stored in secrets.json and should be read from here when needed. 
+    /// </summary>
+    public class Secret
+    {
+        public string Name { get; init; }
+        public string Key { get; init; }
+    }
+}
