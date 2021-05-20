3afa3297b617bdae767a6019cb1055c0110d900d
Geckk0 <jesper95951@gmail.com>
2021-05-20 11:30:19 +0200

    Fixed filtering

--- Holiday_Maker/ClientApp/src/components/Filters/SearchDistanceBeach.vue
+++ Holiday_Maker/ClientApp/src/components/Filters/SearchDistanceBeach.vue
@@ -4,7 +4,8 @@
         
         <input type="range" min="0" max="5000" step="100" class="search-box" v-model="DistanceBeach" @click="handleSubmit">
         
-        <h4 v-if="DistanceBeach > 1" class="slider-stars">0 - {{DistanceBeach}}m</h4>
+        <h4 v-if="DistanceBeach == 5000" class="slider-stars">Any distance</h4>
+        <h4 v-else-if="DistanceBeach > 0" class="slider-stars">0 - {{DistanceBeach}}m</h4>
         <h4 v-else class="slider-stars">Distance not counted</h4>
     </div>
 </template>
@@ -13,7 +14,7 @@
 export default{
     data() {
         return {
-            DistanceBeach: 2500
+            DistanceBeach: 5000
         }
     },
     methods: {
