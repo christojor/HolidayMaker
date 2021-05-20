73525193f7806fcd8aa38288928ace6a6d1e742d
Johan Rova <johan.rova@protonmail.com>
2021-05-20 15:57:30 +0200

    Skeleton for Stripe functionality is now here. I made a successfull testpay in swagger. Just gotta collect information now and remove hardcoded trash

--- Holiday_Maker/ClientApp/src/components/pages/PayUpSucka.vue
+++ Holiday_Maker/ClientApp/src/components/pages/PayUpSucka.vue
@@ -8,13 +8,15 @@
       </label>
       <button type="submit">Submit payment</button>
     </form>
-    <button @click="logToConsole">LMAO</button>
+    <button v-on:click="lolgsomeshit">click me </button>
   </div>
 </template>
 
 <script>
 export default {
   mounted: function () {
+    console.log("lmao");
+    this.logToConsole;
     //Building payment form starts here
     var stripe = Stripe(
       "pk_test_51IsjoPK0RxPPVgejwsizP9ghkzrEOEAho8VjGbz0Rtn2i31J5t5zr6NGp04eZD0ZHF5TwIzvCZf2XFmZR4syWqiY00ldbn6Luv"
@@ -51,7 +53,10 @@ export default {
             name: "Johan Rova",
           },
         })
-        .then(console.log(this.result.paymentMethod));
+        .then(function(result)
+        {
+          console.log(result);
+        });
     });
   },
   methods: {
@@ -77,9 +82,43 @@ export default {
         });
       }
     },
-    logToConsole() {
-      console.log("lmao");
+        lolgsomeshit(){
+      console.log("fucker");
     },
+    logToConsole() {
+      console.log("lmsssssssssssssssssao");
+    }
   },
 };
 </script>
+
+<!--<script>
+export default {
+  methods: {
+    changeText() {
+      // Två in-argument här, den första avser den mutationen man vill utföra i store,
+      // OBS - Måste vara en sträng som har samma namn som mutationen i store.
+      // Det andra argumentet är själva "the payload" - nya värdet man vill skicka med som kommer
+      // att användas när mutationen updaterar data-variablen i state. Den kan vara av vilken datatyp som helst,
+      // men den måste ju stämma överens med den avsedda data-variablen i state.
+
+      this.$store.commit("changeText", "Changed"); // Commit used for calling mutations in store
+    },
+    lolgsomeshit(){
+      console.log("fucker");
+    },
+    getAccomodations() {
+      this.$store.dispatch("getAccomodations"); // Dispatch used for calling actions in store
+    },
+    goToAccommodation(accommodationId) {
+      this.$router.push({
+        name: "Accommodation",
+        params: { id: accommodationId },
+      });
+    },
+    getRooms() {
+      this.$store.dispatch("getRooms");
+    },
+  },
+};
+</script>-->
