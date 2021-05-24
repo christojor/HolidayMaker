79fcc67b82e7263924876eb97a3db46366ea27a3
DESKTOP-7GPI486\kseni <ksenia.gladkikh@outlook.com>
2021-05-24 16:19:37 +0200

    Fixed page navigation in navbar on Accomodation page

--- Holiday_Maker/ClientApp/src/components/details/Navbar.vue
+++ Holiday_Maker/ClientApp/src/components/details/Navbar.vue
@@ -3,20 +3,33 @@
   <div class="flex flex-wrap overflow-hidden">
   <div class="w-1/2 overflow-hidden">
 <ul class="flex">
+  <!-- <div class="text-green-500 hover:text-green-800 hover:underline"> -->
   <li class="mr-6">
-    <a class="text-green-500 hover:text-green-800 hover:underline" href="#">Overview</a>
+    <div ref="overview">
+    <a class="overview-button text-green-500 hover:text-green-800 hover:underline" @click="scrollPageTo('overview')">Overview</a>
+    </div>
   </li>
   <li class="mr-6">
-    <a class="text-green-500 hover:text-green-800 hover:underline" href="#">Amenities</a>
+    <div ref="amenities">
+      <a class="amenities-button text-green-500 hover:text-green-800 hover:underline" @click="scrollPageTo('amenities')">Amenities</a>
+  </div>
   </li>
   <li class="mr-6">
-    <a class="text-green-500 hover:text-green-800 hover:underline" href="#">Extras</a>
+<div ref="extras">
+    <a class="rooms-button text-green-500 hover:text-green-800 hover:underline" @click="scrollPageTo('extras')">Extras</a>
+    </div>  
+    </li>
+  <li class="mr-6">
+    <div ref="rooms">
+    <a class="rooms-button text-green-500 hover:text-green-800 hover:underline" @click="scrollPageTo('rooms')">Available Rooms</a>
+    </div>
   </li>
+  <!-- </div> -->
 </ul>
 </div>
 <div class="w-1/2 overflow-hidden">
-<button class="float-right bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
-      Book Room
+<button class="float-right bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" @click="scrollPageTo('rooms')">
+      Show Rooms
   </button>
   </div>
   </div>
@@ -25,5 +38,23 @@
 
 <script>
 
-export default {};
+export default {
+  data(){
+    return {
+       refName:'room',
+       refName: 'amenities',
+       refName: 'extras',
+       refName: 'overview'
+    }
+  },
+  methods: {
+  scrollPageTo(refName) {
+    
+    var element = this.$refs[refName];
+    var top = element.offsetTop;
+
+    window.scrollTo(0, top);
+  }
+ }
+}
 </script>
