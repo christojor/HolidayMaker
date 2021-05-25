da713f1d1a6e579f049dec36912e1608b145f114
samykiiskinen <samykiiskinen@gmail.com>
2021-05-25 10:27:00 +0200

    fixing MyBooking and MyPage

--- Holiday_Maker/ClientApp/src/components/pages/MyBookings.vue
+++ Holiday_Maker/ClientApp/src/components/pages/MyBookings.vue
@@ -1,32 +1,37 @@
 <template>
     <h2 class="header bg-green-2">My Bookings</h2>
     <table>
-        <tr class="tableHeader bg-green-1">
+        <tr class="bg-green-1">
             <th class="headerElements">BookingId</th>
             <th class="headerElements">Accommodation</th>
             <th class="headerElements">Check in Date</th>
             <th class="headerElements">Check out Date</th>
             <th class="headerElements">Price</th>
         </tr>
-        <tr class="bookingsList">
+        <tr>
             <td class="bookingElements">1234</td>
             <td class="bookingElements">Hotel</td>
             <td class="bookingElements">Today</td>
             <td class="bookingElements">Tomorrow</td>
             <td class="bookingElements">1000</td>
-            <td><button class="bg-green-2" style="padding-left:15px; padding-right:10px; font-weight:bold; text-align:center">Edit</button></td>
-            <td><button class="bg-green-3" style="padding-left:10px; padding-right:10px; font-weight:bold">Cancel</button></td>
+            <td><button class="editButton">Edit</button></td>
+            <td><button class="editButton">Cancel</button></td>
         </tr>
     </table>  
+        <div class="container2 bg-green-1 shadow-md" style="height:70px">
+            <router-link to="/MyPage" class="myBookings-button bg-green-500 hover:bg-green-700 py-3 px-4 rounded-full shadow-xl">My Page</router-link>
+        </div>
 </template>
 
 
 <script>
 import Booking from '/src/components/pages/Booking.vue'
+import MyPage from '/src/components/pages/MyPage.vue'
 
 export default {
     components:{
         Booking,
+        MyPage,
     },
 }
 </script>
--- Holiday_Maker/ClientApp/src/components/pages/MyPage.vue
+++ Holiday_Maker/ClientApp/src/components/pages/MyPage.vue
@@ -1,6 +1,24 @@
 <template>
-    <h2>My Page</h2>
+    <h2 class="header bg-green-2">My Page</h2>
     <div class="container2 bg-green-1 shadow-md" style="height:70px">
         <router-link to="/MyBookings" class="myBookings-button bg-green-500 hover:bg-green-700 py-3 px-4 rounded-full shadow-xl">My Bookings</router-link>
     </div>
+        <table>
+        <tr class="bg-green-1">
+            <th class="headerElements">Title</th>
+            <th class="headerElements">Name</th>
+            <th class="headerElements">Address</th>
+            <th class="headerElements">e-mail</th>
+            <th class="headerElements">Phone number</th>
+            <th class="headerElements">Member type</th>
+        </tr>
+        <tr>
+            <td class="bookingElements">Mrs.</td>
+            <td class="bookingElements">Diana Cross</td>
+            <td class="bookingElements">1st Avenue</td>
+            <td class="bookingElements">crisscross@loosypants.com</td>
+            <td class="bookingElements">1564784321</td>
+            <td class="bookingElements">Golden fever</td>
+        </tr>
+    </table> 
 </template>
