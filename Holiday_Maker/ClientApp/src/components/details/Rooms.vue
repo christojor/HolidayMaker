6074d5fc30d4b2a7b401f8a474ff6e51ec0bd546
DESKTOP-7GPI486\kseni <ksenia.gladkikh@outlook.com>
2021-06-01 16:07:34 +0200

    Test added chekboxes with Extras and prices

--- Holiday_Maker/ClientApp/src/components/details/Rooms.vue
+++ Holiday_Maker/ClientApp/src/components/details/Rooms.vue
@@ -1,49 +1,101 @@
 <template>
-<b>AVAILABLE ROOMS</b>
-<div v-for="room in rooms" :key="room">
-<div class="flex flex-wrap overflow-hidden border-black border-2 p-1 m-1">
+  <b>AVAILABLE ROOMS</b>
+  <div v-for="room in rooms" :key="room">
+    <div class="flex flex-wrap overflow-hidden border-black border-2 p-1 m-1">
+      <div class="w-1/2 overflow-hidden p-2">
+        <img class="rounded-md roomImg" :src="room.imgSrc" />
+      </div>
 
-  <div class="w-1/2 overflow-hidden p-2">
-    <img class="rounded-md roomImg" :src="room.imgSrc">
-   </div>
+      <div class="w-1/2 overflow-hidden p-4">
+        <button
+          class="float-right bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
+          @click="addToBooking(room)"
+        >
+          Book Room
+        </button>
+        <div>
+          <b>Room Type:</b> {{ room.roomType.name }}<br />
+          <b>Number of Guests: </b> {{ room.nbrOfGuests }}<br />
+          <b>Number of Extra Beds:</b> {{ room.nbrOfExtraBeds }}<br />
+          <b>Price:</b> {{ room.price }}€ <br />
+          <div v-if="room.isRefundable == true"><b>Refund:</b> Yes<br /></div>
+          <div v-if="room.isRefundable == false"><b>Refund:</b> No<br /></div>
+          <b>Description:</b> {{ room.description }}<br />
+          <b>Extras at extra price: </b>
+          <div class="rounded-t-md bg-green-1 search-div shadow-xl w-2/8">
+            <div class="text-green-500">
+               <div class="selectBox" @click="toggledisplay">
+                    <select>
+                        <option>Select Extras</option>
+                    </select>
+                    <div class="overSelect"></div>
+            </div>
+              <div
+                :class="{ show: toggle }"
+                id="checkboxes"
+                @click="handleSubmit"
+              >
+                <div
+                  v-for="Extra in Extras"
+                  :key="Extra"
+                  class="flex items-center"
+                >
+                  <input
+                    type="checkbox"
+                    :id="Extra.Extra"
+                    v-model="Extra.Checked"
+                  />
+                  <label :for="Extra.Extra">&nbsp;{{ Extra.Extra }}, price: {{Extra.Price}}</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
-   <div class="w-1/2 overflow-hidden p-4">     
-     <button class="float-right bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" @click="addToBooking(room)">
-      Book Room
-     </button> 
-     <div>
-     <b>Room Type:</b> {{ room.roomType.name }}<br>
-     <b>Number of Guests: </b> {{ room.nbrOfGuests }}<br>
-     <b>Number of Extra Beds:</b> {{ room.nbrOfExtraBeds }}<br>
-     <b>Price:</b> {{ room.price }}€ <br>
-     <div v-if="room.isRefundable==true">
-       <b>Refund:</b> Yes<br>
-       </div>
-     <div v-if="room.isRefundable==false">
-     <b>Refund:</b> No<br>
-     </div>
-     <b>Description:</b> {{room.description}}<br>
-    </div>
-    </div>
-</div>
-</div>
-    
+  <!-- <extrasPrices class="extra"></extrasPrices> -->
 </template>
 
-<script>
 
+<script>
+import extrasPrices from "/src/assets/extrasPrices.json";
 export default {
-    props: {
-        rooms: {
-            type: Object,
-            required: true,
-        }
+  data() {
+    return {
+      toggle: true,
+      Extras: [
+        { Extra: "Self-Catering", Checked: false, Price: 5},
+        { Extra: "HalfBoard", Checked: false, Price: 10},
+        { Extra: "FullBoard", Checked: false, Price: 15},
+        { Extra: "All-Inclusive", Checked: false, Price: 20},
+        { Extra: "Breakfast", Checked: false, Price: 5 },
+        { Extra: "ExtraBed", Checked: false, Price: 10},
+        { Extra: "Crib", Checked: false, Price: 10},
+      ],
+    };
+  },
+  props: {
+    rooms: {
+      type: Object,
+      required: true,
     },
-      methods: {
-        addToBooking(room) {
-          this.$store.state.bookedRooms.push(room)
-        }
-}
-    
-}
+  },
+  methods: {
+    addToBooking(room) {
+      this.$store.state.bookedRooms.push(room);
+    },
+    toggledisplay(){
+            this.toggle=!this.toggle
+            console.log("hej")
+        },
+    handleSubmit(){
+            let filter = {
+                PropExtras: this.Extras
+            }
+        },
+
+    }
+};
 </script>
