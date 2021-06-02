42f4977e8252fe27b557f8b7b904e9b5f6bdc2f0
Christoffer Jörgensen <christoffer.joergensen@gmail.com>
2021-06-02 14:09:01 +0200

    LINQ query for finding available rooms.

--- Holiday_Maker/Services/RoomService.cs
+++ Holiday_Maker/Services/RoomService.cs
@@ -14,6 +14,8 @@ namespace Holiday_Maker.Services
         private readonly IGenericRepository<Accomodation> _accomodationRepo;
         private readonly IGenericRepository<AccomodationType> _accomodationTypeRepo;
         private readonly IGenericRepository<RoomType> _roomTypeRepo;
+        private readonly IGenericRepository<Booking> _bookingRepo;
+        private readonly IGenericRepository<BookedRoom> _bookedRoomRepo;
 
         public RoomService()
         {
@@ -21,7 +23,9 @@ namespace Holiday_Maker.Services
             _roomTypeRepo = new GenericRepository<RoomType>();
             _accomodationRepo = new GenericRepository<Accomodation>();
             _accomodationTypeRepo = new GenericRepository<AccomodationType>();
-           
+            _bookingRepo = new GenericRepository<Booking>();
+            _bookedRoomRepo = new GenericRepository<BookedRoom>();
+
         }
 
 
@@ -63,6 +67,42 @@ namespace Holiday_Maker.Services
                 accoRoom.RoomType = await _roomTypeRepo.GetById(accoRoom.RoomTypeId);
             }
             return accomodationsRooms;
+        }
+        public async Task<IEnumerable<int>> AvailbleRoomIds(DateTime checkInDate, DateTime checkOutDate)
+        {
+            var rooms = _roomRepo.GetAllRaw();
+            var bookedRooms = _bookedRoomRepo.GetAllRaw();
+            var bookings = _bookingRepo.GetAllRaw();
+
+            var availableRoomIds =
+                from room in rooms
+                where !(from //join here)
+                select room.Id;
+
+
+            from B in bookings
+            join BR in bookedRooms
+                on B.Id equals BR.BookingId
+            where (B.CheckInDate <= checkOutDate && B.CheckOutDate >= checkInDate)
+           OR(CheckInDate < @CheckOutDate AND CheckOutDate >= @CheckOutDate)
+           OR(@CheckInDate <= CheckInDate AND @CheckOutDate >= CheckInDate)
+            select BR.RoomId
+
+SELECT Id
+FROM Room
+WHERE Room.Id NOT IN
+(
+    SELECT RoomId
+    FROM   Booking B
+            JOIN BookedRoom BR
+               ON B.Id = BR.BookingId
+    WHERE(CheckInDate <= @CheckOutDate AND CheckOutDate >= @CheckInDate)
+           OR(CheckInDate < @CheckOutDate AND CheckOutDate >= @CheckOutDate)
+           OR(@CheckInDate <= CheckInDate AND @CheckOutDate >= CheckInDate)
+)
+
+            return availableRoomIds;
+
         }
     }
 }
